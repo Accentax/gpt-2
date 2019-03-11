fe4596792afcce2c5beb9866e98eb9d60d2cb93b
alexanderleirvag <bakos88@gmail.co>
2019-03-11 11:00:20 +0100

    add

---
+++ data/donwload.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://dl.fbaipublicfiles.com/fairseq/data/writingPrompts.tar.gz
+tar xvzf writingPrompts.tar.gz
---
+++ data/makedata.py
@@ -0,0 +1,9 @@
+with open("train.wp_source") as file: # Use file to refer to the file object
+    prompt = file.readlines()
+with open("train.wp_target") as file: # Use file to refer to the file object
+    response = file.readlines()
+compiled=""
+for i in range(10000):
+    compiled+=prompt[i]+" [WP-START] "+response[i]+" [WP-END] "
+f = open("train_10000.txt", "w")
+    f.write(compiled)
